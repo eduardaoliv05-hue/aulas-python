699b64d33ddb079b469e20cf18c7ae3ae25f599c
eduarda <eduardaoliv05@gmail.com>
2026-02-15 09:02:41 -0300

    conteudo sobre loops

--- modulo-01/conteudo-python/codigos.py
+++ modulo-01/conteudo-python/codigos.py
@@ -17,3 +17,22 @@ print(lista)
 lista.append("cavalo")
 print(lista)
 # endregion
+
+# region - tipos de loops
+#range roda o loop dentro de um raio, for cria uma condição ao loop
+for n in range(1, 11):
+  print(f"this is a loop {n}")
+
+# while roda o loop enquanto ele for true, while true roda o loop infinitamente
+contador = 0
+while contador <=10:
+    print(contador)
+    contador +=1
+
+
+# region - isalpha(): = identifica se todos os caracteres sao letras
+texto = "abc123"
+for c in texto:
+    if c.isalpha():
+     print(c, "é letra")
+# endregion
